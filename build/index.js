bc3fd6282ae5b27a66686dadd680a3d1e16c7783
ProfessorDy <kingdeeyfx@gmail.com>
2023-06-01 19:16:26 +0100

    preventDefault()

--- build/index.js
+++ build/index.js
@@ -1,11 +1,19 @@
-const submitBtn = document.querySelector('.submit-btn')
-const ratingCard = document.getElementById('rating-card')
-const thankYouCard = document.getElementById('thank-you-card')
+const form = document.querySelector('.rating-form')
+const ratingCard = document.querySelector('#rating-card')
+const thankYouCard = document.querySelector('#thank-you-card')
+const cards = document.querySelectorAll('.card')
 
+console.log(ratingCard) 
+console.log(thankYouCard) 
+const changeState = (e) => {
+    e.preventDefault()
+    cards.forEach(card => {
+        card.classList.toggle('hide')
+    });
 
+    const data = new FormData(e.target)
+    console.log(data)
+}
 
-
-submitBtn.addEventListener('click', () => {
-    ratingCard.classList.add('hide')
-    thankYouCard.classList.remove('hide')
-})
+form.addEventListener('submit', changeState)
+console.log(form);
